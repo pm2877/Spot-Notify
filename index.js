520e0009c2731a7679c81882feaafbaac859cab9
Parth Mehta <parth94@hotmail.com>
2017-03-11 17:32:15 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const notifier = require('node-notifier');
+var spotify = require('spotify-node-applescript');
+
+spotify.getTrack(function(err, track){
+
+	notifier.notify({
+	title: track.album + ' by ' + track.artist,
+    message: track.name
+	});
+	// console.log(track.name)
+    /*
+    track = {
+        artist: 'Bob Dylan',
+        album: 'Highway 61 Revisited',
+        disc_number: 1,
+        duration: 370,
+        played count: 0,
+        track_number: 1,
+        starred: false,
+        popularity: 71,
+        id: 'spotify:track:3AhXZa8sUQht0UEdBJgpGc',
+        name: 'Like A Rolling Stone',
+        album_artist: 'Bob Dylan',
+        artwork_url: 'http://images.spotify.com/image/e3d720410b4a0770c1fc84bc8eb0f0b76758a358',
+        spotify_url: 'spotify:track:3AhXZa8sUQht0UEdBJgpGc' }
+    }
+    */
+
+});
+
