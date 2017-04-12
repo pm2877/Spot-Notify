9bf5a95c88e7c9b92ba091537077ae9d545263fd
Parth Mehta <parth94@hotmail.com>
2017-04-12 18:37:32 -0400

    backup

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const notifier = require('node-notifier');
 const path = require('path');
+const EventEmitter = require('events'); 
 const cmd = require('node-cmd');
+var memwatch = require('memwatch-next');
 var spotify = require('spotify-node-applescript');
 var isSpotifyRunning
 var initialState
 var isNotified = false
 var trackAlbum, trackAlbumArtist, trackName, artworkUrl, trackPopularity
 var oldTrackName=''
-/*TODO:
-1. Fix bug that is causing no notification being displayed for the first time when song is played where spotify is started after it was quit while a track was playing
 
-2. destroy notifactions.
+/*TODO:
 
 */
 
-
+EventEmitter.defaultMaxListeners = 0
 getRunningStatus(getState)
 
 function getRunningStatus(getState){
@@ -31,14 +31,14 @@ function setRunningStatus(isRunning){
         getState(detectStateChange) //pass a callback
     }
     else{
-        getRunningStatus(getState)
+        setTimeout.bind(null, getRunningStatus(getState), 1000)
     } 
 }
 
 function getState(detectStateChange){        //takes a callback
     spotify.getState(function(err, state){
         if(state==null || state == undefined){
-            getRunningStatus(getState)
+            setTimeout.bind(null, getRunningStatus(getState), 1000)
         }
         else{
             currentState = state.state
@@ -48,27 +48,29 @@ function getState(detectStateChange){        //takes a callback
 }
 
 function detectStateChange(newState){
-    if(newState == 'playing'){
+    if(newState == 'playing'){        //TODO: maybe can use a variable oldState and set it to false every time you notify? and check here if oldState != newState
         getTrackDetails(notify)
         isNotified = true
     }
     else{
         isNotified = false
     }
-    setTimeout.bind(null, getState(detectStateChange), 10)             // This is the main recursive call. setTimeour.bind() clears the call stack and minimizes memory use.
+    setTimeout.bind(null, getState(detectStateChange), 1000)             // This is the main recursive call. setTimeour.bind() clears the call stack and minimizes memory use.   
 }
 
 function getTrackDetails(notify){
     spotify.getTrack(function(err, track){
-        if(track==null || track==undefined){
-            getState(detectStateChange)
+        if(track==null || track==undefined){ 
+            setTimeout.bind(null, getState(detectStateChange), 1000)
         }
         else{
             //console.log(track.popularity)
             setTrackDetails(track.album, track.album_artist, track.name, track.artwork_url, track.popularity)
+            
         }      
     });
     notify()
+    
 }
 
 function setTrackDetails(album, album_artist, name, artwork, popularity){
@@ -80,8 +82,12 @@ function setTrackDetails(album, album_artist, name, artwork, popularity){
     // artworkUrl = artwork
 }
 
+memwatch.on('leak', function(info) {
+ console.log(info)
+});
+
 function notify(){
-    if(!isNotified || oldTrackName!=trackName){  //Notify only once
+    if((!isNotified || oldTrackName!=trackName)&&trackName!=undefined){  //Notify only once
         //console.log("Notify")
 
         // cmd.run('terminal-notifier -title ' + trackName + ' -subtitle ' + trackAlbum + ' -message ' + trackAlbumArtist + ' -group "com.spotify.client" -activate "com.spotify.client"');
@@ -90,12 +96,13 @@ function notify(){
         notifier.notify({
             title: trackName,
             subtitle: trackAlbum,
+            // icon: path.join(__dirname, 'spotify-logo.png'),
             contentImage: path.join(__dirname, 'spotify-logo.png'),
             icon: path.join(__dirname, 'play-music-icon.png'),
             message: trackAlbumArtist + ' --- Popularity: ' + trackPopularity,
             sender: 'com.spotify.client',
             group: 'com.spotify.client',
-            actions: 'skip'
+            actions: 'Skip'
         }
         // ,function() {
         //     console.log(arguments);
@@ -109,9 +116,11 @@ function notify(){
             // if(arguments['2'].activationType=='contentsClicked'){
             //     cmd.run('open -a Spotify')            // will work on only Mac OS and Linux
             // }
-            if(arguments['2'].activationValue=='skip'){
-                console.log('open -a Spotify')  //Skip to next song here
-            }
+
+            //Don't need this right now, but useful method
+            // if(arguments['2'].activationValue=='skip'){
+            //     console.log('open -a Spotify')  //Skip to next song here
+            // }
         });
         oldTrackName = trackName
     } 
